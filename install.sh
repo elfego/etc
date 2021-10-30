ed04bbce42afb7ec81a229b39a9278c6465b2f84
Elfego Ruiz Gutierrez <elfegoruiz@gmail.com>
2021-10-30 18:38:27 +0100

    generalised for the conda for any user

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-
 ln -s ${PWD}/vimrc ${HOME}/.vimrc
 
 mkdir -p ${HOME}/.config/nvim
 ln -s ${PWD}/init.vim ${HOME}/.config/nvim/init.vim
 
+sed 's@XXXX@'"$HOME"'@g' conda_init.template > conda_init.sh
 ln -s ${PWD}/zshrc ${HOME}/.zshrc
-
-
-
 
